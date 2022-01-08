656048ff54ed410eb3dced3635248f4c51d54564
asett0 <amitsett0@gmail.com>
2022-01-08 14:03:37 +0800

    added pig latin function and reformatted summary stats functions

--- pig_latin/src/main.rs
+++ pig_latin/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let phrase = "the sky is blue";
+    let pig_phrase = phrase
+        .split(" ")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&word| pig(word))
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    println!(
+        "The pharse \"{}\" in pig latin is \"{}\" ",
+        phrase, pig_phrase
+    );
+}
+
+fn pig(word: &str) -> String {
+    if word != "" && word.chars().all(|c| ('a'..='z').contains(&c)) {
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+        if vowels.contains(&word.chars().nth(0).unwrap()) {
+            format!("{}-hay", word)
+        } else {
+            format!(
+                "{}-{}ay",
+                word.chars().skip(1).collect::<String>(),
+                word.chars().nth(0).unwrap()
+            )
+        }
+    } else {
+        if word == "" {
+            panic!("Empty string provided to pig function.")
+        } else {
+            panic!(
+                "String with non-lowercase english alphabet letters provided pig function: {}",
+                word
+            )
+        }
+    }
 }
--- summary_stats/src/main.rs
+++ summary_stats/src/main.rs
@@ -1,8 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
-
-    let input  = vec![1,1,20,5,10,20];
+    let input = vec![1, 1, 20, 5, 10, 20];
 
     println!("The mean of {:?} is {:?}", input, mean(&input));
     println!("The median of {:?} is {:?}", input, median(&input));
@@ -10,57 +9,46 @@ fn main() {
 }
 
 fn mean(ints: &Vec<i32>) -> f64 {
-    
     if let 0 = ints.len() {
         panic!("Empty vector provided to mean function")
-    }
-    else {
-
+    } else {
         let mut total = 0;
         for int in ints.iter() {
             total += int
         }
-    
-        total as f64/ints.len() as f64
-    }
 
+        total as f64 / ints.len() as f64
+    }
 }
 
 fn median(ints: &Vec<i32>) -> f64 {
     if let 0 = ints.len() {
         panic!("Empty vector provided to median function")
-    }
-    else {
+    } else {
         let mut sorted_ints = ints.clone();
 
         sorted_ints.sort();
-    
+
         let i = sorted_ints.len() / 2;
-    
+
         if let 0 = sorted_ints.len() % 2 {
-            ( sorted_ints[i-1] as f64 + sorted_ints[i] as f64) / 2.
-        }   
-        else {
+            (sorted_ints[i - 1] as f64 + sorted_ints[i] as f64) / 2.
+        } else {
             sorted_ints[i] as f64
-        } 
+        }
     }
-
-
-
 }
 fn mode(ints: &Vec<i32>) -> i32 {
-
     if let 0 = ints.len() {
         panic!("Empty vector provided to mode function")
-    }
-    else {
+    } else {
         let mut counts = HashMap::new();
 
         for int in ints.iter() {
             let count = counts.entry(int).or_insert(0);
             *count += 1;
         }
-    
+
         let mut max = 0;
         let mut mode = None;
         for (&k, &v) in counts.iter() {
@@ -68,12 +56,10 @@ fn mode(ints: &Vec<i32>) -> i32 {
                 max = v;
                 mode = Some(*k).clone();
             }
-            
         }
         if let Some(value) = mode {
-            value  
-        } 
-        else {
+            value
+        } else {
             panic!("Empty vector provided to mode function")
         }
     }
